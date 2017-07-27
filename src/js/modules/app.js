1ed4d95a100e60d322f24d2cbb4ca186842d4737
platonovAlexey <kalyawe4ka@gmail.com>
2017-07-27 17:35:57 +0300

    add change tabs

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -14,7 +14,7 @@ $('[id^="cards_"]').each(function() {
 		var date = new Date($(this).attr('data')),
 		now = new Date();
 		day = now.getDay();
-		date = new Date(date.setHours(16));
+		date = new Date(date.setHours(20)); //16
 		forecast = $('.tabs__wrapper__online');
 		completed = $('.tabs__wrapper__offline');
 		col = $('.col-lg-3.col-md-3.col-sm-6.col-xs-15');
@@ -27,25 +27,30 @@ $('[id^="cards_"]').each(function() {
 		if (date < now) {
 console.log(format(date), 'прошлое')
 		// Это прошлые даты
-		// $(this).addClass('card__completed').css("display", "block");
-		$(this).addClass('card-completed');
+		$(this).addClass('card-completed').css("display", "block");
+		//$(this).addClass('card-disaeble').css("display", "block");
+		$(this).addClass('card-active').css("display", "block");
 		// $(this).removeClass('card__disable');
-		$(this).removeClass('card-active');
-		$(this).removeClass('card-default');
+		//$(this).removeClass('card-active');
+		//$(this).removeClass('card-default');
 		//$(this).removeClass('card__default');
-		$(this).append('<div class="card-bottom__link">Завершено</div>');
+		// $(this).append('<div class="card-bottom__link">Завершено</div>');
 		$(this).find(".card-bottom__link").html("Завершено");
 		$(this).find(".card-middle__promocode").html("");
+
 		forecast.find(col).find('.card-completed').parent().parent().remove();
 
-		//completed.find(col).find('.card__active').parent().parent().remove();
-				
-		// forecast.find(col).find('.card__completed').addClass('removeClass');
+		//completed.find(col).find('.card-default').parent().parent().remove();
+
+		//forecast.find(col).find('.card__completed').addClass('removeClass');
 		// $(this).remove();
 		
 		
 		//forecast.find('.removeClass').remove();
-		// $('.tabs__wrapper__online').find('.card__active').css("display", "block");
+		completed.find('.card-default').css("display", "block");
+		completed.find('.card-default').removeClass('card-active');
+
+		
 
 		
 		
@@ -62,27 +67,20 @@ console.log(format(date), 'будущее')
 		$(this).removeClass('card-completed');
 		$(this).removeClass('card-active');
 		$(this).removeClass('card-disable');
-		// $(this).addClass('card-default');
+		$(this).addClass('card-default');
 		$(this).addClass('card-disable');
-		// $(this).addClass('card-active');
+		
+
+		
 		$(this).find(".card-middle__promocode").html("");
 		$(this).find(".card-bottom__link").html("Ожидается");
 
-		completed.find(col).find('.card-default').parent().parent().remove();
+		completed.find(col).find('.card-active').parent().parent().remove();
+		completed.find(col).find('.card-disable').parent().parent().remove();
 
-		// var everyDays = everyday.find('.col-lg-18').find('.card-wrapper').find('.card-disable').addClass('card-active');
-		// everyDays.addClass(function(index) {
-		// 	return "item-" + index;
-		// });
+		// completed.find('.card-completed').css("display", "block");
 
-		var every = everyday.find('.card-default:first').addClass('card-everyday').addClass('card-active').removeClass('card-disable');
-		var nextEvery = everyday.find('.card-disable').addClass('active1');
-		if ($(this).hasClass('active1')) {
-			$(this).parent().parent().remove();
-		}
-		// if (every.hasClass('bla')) {
-		// 	$(this).addClass('card-active');
-		// }
+		
 
 		//completed.find('.card-completed').css("display", "none");
 		// completed.find('.card-default').css("display", "none");
@@ -93,13 +91,39 @@ console.log(format(date), 'будущее')
 if(format(now) === $(this).attr('data')) {
 console.log(format(date), 'сегодня')
 		$(this).removeClass('card-completed');
-		$(this).removeClass('card-default');
-		$(this).removeClass('card-disable');
+		//$(this).removeClass('card-default');
+		//$(this).removeClass('card-disable');
 		$(this).addClass('card-active');
-		$(this).find(".card-bottom__link").html("Перейти к покупке>");
-		$(this).find(".completed").remove();
+		$(this).find(".card-bottom__link").html("Перейти к покупке");
+		//$(this).find(".completed").remove();
 		$(this).find(".card-middle__promocode").css("display", "block");
 		//$(this).find(".card__link").css("fontSize", 14 );
+
+
+
+
+
+		// var everyDays = everyday.find('.col-lg-18').find('.card-wrapper').find('.card-default').addClass('card-active');
+		// everyDays.removeClass('card-completed');
+		// everyDays.removeClass('card-default');
+
+		// everyday.find('.col-lg-18').find('.card-wrapper').find('.card-active').css("display", "block");
+
+		// everyDays.removeClass('card-disable');
+
+
+		// everyDays.addClass(function(index) {
+		// 	return "item-" + index;
+		// });
+
+		// var every = everyday.find('.card-default:first').addClass('card-everyday').addClass('card-active').removeClass('card-disable');
+		// var nextEvery = everyday.find('.card-disable').addClass('active1');
+		// if ($(this).hasClass('active1')) {
+		// 	$(this).parent().parent().remove();
+		// }
+		// if (every.hasClass('bla')) {
+		// 	$(this).addClass('card-active');
+		// }
 
 
 
@@ -113,21 +137,6 @@ var linkOnline = $('#tab-nav-2');
 var linkOffline = $('#tab-nav-1');
 var tabWrapper = $('.tabs__wrapper');
 
-// var item = forecast.find('.card__default').map(function(index, elem) {
-// 	return $(elem).attr('data');
-// })
-// var arr = item.get();
-// var arrParse = parseIntar(arr);
-
-// function sum(arr){
-// 	if(arr[i] > arr[i]+1){
-// 		return console.log('больше');
-// 	}
-	
-// }
-// console.log(sum);
-
-// console.log(item);
 
 linkOnline.click(function(){
 	console.log('click online');
